70134a2ee4c77fc2836db970de4293d07c9a4846
Jingxiang Zhang <jz3313@columbia.edu>
2025-12-22 16:32:57 -0800

    refactor: restructure the os examples

---
+++ Operating System/examples/02-synchronization/barrier.cpp
@@ -0,0 +1,21 @@
+/*
+ * barrier.cpp
+ * 
+ * Demonstrates barrier synchronization in C++.
+ * Shows how to coordinate multiple threads to wait at a common point.
+ * 
+ * Compilation: g++ -std=c++11 -o barrier barrier.cpp -pthread
+ * Usage: ./barrier
+ */
+
+#include <iostream>
+#include <thread>
+#include <pthread.h>
+
+int main() {
+    // TODO: Implement barrier synchronization in C++
+    std::cout << "Barrier example (C++) - To be implemented\n";
+    
+    return 0;
+}
+
---
+++ Operating System/examples/02-synchronization/semaphore.cpp
@@ -0,0 +1,22 @@
+/*
+ * semaphore.cpp
+ * 
+ * Demonstrates semaphores in C++ (using POSIX or C++20 std::counting_semaphore).
+ * Shows both binary and counting semaphores.
+ * 
+ * Compilation: g++ -std=c++11 -o semaphore semaphore.cpp -pthread
+ * Usage: ./semaphore
+ */
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <semaphore.h>  // POSIX semaphore
+
+int main() {
+    // TODO: Implement semaphore examples in C++
+    std::cout << "Semaphore example (C++) - To be implemented\n";
+    
+    return 0;
+}
+
